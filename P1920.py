833dac0929a818c273687684832fcbcf94a47e6b
김경오 <kimgyeong-o@gimgyeong-oui-MacBookPro.local>
2020-09-20 00:53:59 +0900

    알고리즘풀이

---
+++ P1920.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr_n = set(map(int, input().split(" ")))
+m = int(input())
+arr_m = list(map(int, input().split(" ")))
+for i in arr_m:
+    if i in arr_n:
+        print('1')
+    else:
+        print('0')
