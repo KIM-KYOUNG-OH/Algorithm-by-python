79852d7b8908ddbca7c190206b3a4c671127afb3
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2020-11-24 00:23:26 +0900

    알고리즘풀이

--- P1697.py
+++ P1697.py
@@ -1,18 +1,23 @@
+from sys import stdin
 from collections import deque
 
-MAX = 100001
-n,k = map(int, input().split())
-array=[0]*MAX
 
-def bfs():
-    q=deque([n])
+def bfs(result, visit, n, k):
+    q = deque()
+    q.append(n)
+    visit[n] = True
     while q:
-        now_pos = q.popleft()
-        if now_pos == k:
-            return array[now_pos]
-        for next_pos in (now_pos - 1, now_pos + 1, now_pos * 2):
-            if 0 <= next_pos < MAX and not array[next_pos]:
-                array[next_pos] = array[now_pos] + 1
-                q.append(next_pos)
+        i = q.popleft()
+        if i == k:
+            return result[i]
+        for j in (i+1, i-1, i*2):
+            if 0 <= j < 100001 and visit[j] == False:
+                visit[j] = True
+                q.append(j)
+                result[j] = result[i] + 1
 
-print(bfs())
+
+n, k = map(int, stdin.readline().split())
+visit = [False] * 100001
+result = [0] * 100001
+print(bfs(result, visit, n, k))
