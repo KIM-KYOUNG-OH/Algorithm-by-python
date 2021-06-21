a8a69be92431f44b7f33e83035cbe5e1e672e729
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-06-22 00:06:59 +0900

    알고리즘풀이

--- current_week/P10973_이전 순열.py
+++ current_week/P10973_이전 순열.py
@@ -9,17 +9,15 @@ def prev_permutation(lst):
             x = i
             break
     if x == -1:
-        return -1
+        return [-1]
     for j in range(l - 1, x, -1):
         if lst[x] > lst[j]:
             lst[x], lst[j] = lst[j], lst[x]
-    return lst[:x + 1] + lst[:x: -1]
+            break
+    return lst[:x + 1] + lst[:x:-1]
 
 
 n = int(sys.stdin.readline().rstrip())
 case = list(map(int, sys.stdin.readline().rstrip().split()))
 answer = prev_permutation(case)
-if answer == -1:
-    print(-1)
-    exit(0)
 print(' '.join(map(str, answer)))
