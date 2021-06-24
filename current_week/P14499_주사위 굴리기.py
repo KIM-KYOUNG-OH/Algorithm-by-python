3726a15395ba1a5ae3a1b82e63cee1135fb28041
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-06-25 01:54:01 +0900

    알고리즘풀이

--- current_week/P14499_주사위 굴리기.py
+++ current_week/P14499_주사위 굴리기.py
@@ -1,26 +1,42 @@
 import sys
 
 
-def move(dir):
-    if dir == 1:  # 동
-        dice[1], dice[3], dice[6], dice[4] = dice[4], dice[1], dice[3], dice[6]
-    if dir == 2:  # 서
-        dice[1], dice[3], dice[6], dice[4] = dice[3], dice[6], dice[4], dice[1]
-    if dir == 3:  # 
+def move_dice(direction):
+    if direction == 1:
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
+    elif direction == 2:
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+    elif direction == 3:
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+    elif direction == 4:
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
 
 
-n, m, x, y, k = map(int, sys.stdin.readline().rstrip().split())
-Map = []
-for _ in range(n):
-    Map.append(list(map(int, sys.stdin.readline().rstrip().split())))
+def dire(direction):
+    if direction == 1:
+        return 0, 1
+    elif direction == 2:
+        return 0, -1
+    elif direction == 3:
+        return -1, 0
+    elif direction == 4:
+        return 1, 0
+
+
+n, m, y, x, k = map(int, sys.stdin.readline().rstrip().split())
+arr = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
 orders = list(map(int, sys.stdin.readline().rstrip().split()))
+dice = [0, 0, 0, 0, 0, 0, 0]
 
-dice = [0] * 7  # [0, 위, 북, 동, 서, 남, 밑]
-
-dy = [0, 0, 0, -1, 1]  # [0, 동, 서, 북, 남]
-dx = [0, 1, -1, 0, 0]
-
-for i in range(len(orders)):
-    ny = y + dy[orders[i]]
-    nx = x + dx[orders[i]]
-    if 0 <= ny < n and 0 <= nx < m:
+for i in orders:
+    a, b = dire(i)
+    if 0 <= y + a < n and 0 <= x + b < m:
+        y += a
+        x += b
+        move_dice(i)
+        if arr[y][x] != 0:
+            dice[1] = arr[y][x]
+            arr[y][x] = 0
+        else:
+            arr[y][x] = dice[1]
+        print(dice[6])
