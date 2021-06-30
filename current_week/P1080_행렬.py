da8040ca0b9ad790f0340c5384c661f0fa5d2006
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-06-30 18:44:58 +0900

    알고리즘풀이

---
+++ current_week/P1080_행렬.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def convert_matrix(r, c, matrix):
+    for i in range(r, r + 3):
+        for j in range(c, c + 3):
+            matrix[i][j] = 1 - matrix[i][j]
+
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+standard_matrix = []
+change_matrix = []
+for _ in range(n):
+    standard_matrix.append(list(map(int, list(sys.stdin.readline().rstrip()))))
+for _ in range(n):
+    change_matrix.append(list(map(int, list(sys.stdin.readline().rstrip()))))
+convert_cnt = 0
+
+for i in range(n - 2):
+    for j in range(m - 2):
+        if standard_matrix[i][j] != change_matrix[i][j]:
+            convert_matrix(i, j, change_matrix)
+            convert_cnt += 1
+
+for i in range(n):
+    for j in range(m):
+        if standard_matrix[i][j] != change_matrix[i][j]:
+            print(-1)
+            exit(0)
+print(convert_cnt)
