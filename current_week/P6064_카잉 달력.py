87e3388e14c029421968f2e07d043881af18abe7
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-06 21:52:28 +0900

    알고리즘풀이

--- current_week/P6064_카잉 달력.py
+++ current_week/P6064_카잉 달력.py
@@ -1,20 +1,15 @@
 import sys
 
+
+def solve(m, n, x, y):
+    while x <= (m * n):
+        if (x - y) % n == 0:
+            return x
+        x += m
+    return -1
+
+
 t = int(sys.stdin.readline().rstrip())
 for _ in range(t):
-    m, n, x, y = map(int, sys.stdin.readline().rstrip().split())
-    k, nx, ny = 0, 0, 0
-    while True:
-        k += 1
-        nx += 1
-        ny += 1
-        if nx > m:
-            nx = 1
-        if ny > n:
-            ny = 1
-        if nx == x and ny == y:
-            break
-        if nx == m and ny == n:
-            k = -1
-            break
-    print(k)
+    M, N, X, Y = map(int, sys.stdin.readline().rstrip().split())
+    print(solve(M, N, X, Y))
