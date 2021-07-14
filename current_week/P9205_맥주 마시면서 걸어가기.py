f8cead07d783a6cb877453011c6b21fbb03eba3f
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-14 23:46:03 +0900

    알고리즘풀이

---
+++ current_week/P9205_맥주 마시면서 걸어가기.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def is_movable(stores, x, y):
+    if len(stores) == 0:
+        return False
+    if (end_x - x) + (end_y - y) <= 1000:
+        return True
+    store_x, store_y = stores.pop()
+    diff_x = store_x - x
+    diff_y = store_y - y
+    if diff_x + diff_y <= 1000:
+        return is_movable(stores, store_x, store_y)
+    else:
+        return False
+
+
+t = int(sys.stdin.readline().rstrip())
+for _ in range(t):
+    n = int(sys.stdin.readline().rstrip())
+    start_x, start_y = map(int, sys.stdin.readline().rstrip().split())
+    stores = []
+    for _ in range(n):
+        store_x, store_y = map(int, sys.stdin.readline().rstrip().split())
+        stores.append((store_x, store_y))
+    end_x, end_y = map(int, sys.stdin.readline().rstrip().split())
+    stores.sort(key=lambda x: (-x[0], -x[1]))
+    if is_movable(stores, 0, 0):
+        print('happy')
+    else:
+        print('sad')
