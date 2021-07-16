0bd6a625b37432509ec46028173adcac3a893e4f
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-16 23:47:03 +0900

    알고리즘풀이

--- 프로그래머스/해시/완주하지 못한 선수.py
+++ 프로그래머스/해시/완주하지 못한 선수.py
@@ -1,9 +1,28 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if completion[i] != participant[i]: # 순서가 다른 순간 i번째가 답
-            return participant[i]
-    return participant[-1] # 전부 일치하면 마지막에 남은애가 답
+# def solution(participant, completion):
+#     participant.sort()
+#     completion.sort()
+#     for i in range(len(completion)):
+#         if completion[i] != participant[i]: # 순서가 다른 순간 i번째가 답
+#             return participant[i]
+#     return participant[-1] # 전부 일치하면 마지막에 남은애가 답
+#
 
-print(solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]))
+
+def solution(participant, completion):
+    answer = ''
+    completion_dic = dict()
+    for i in completion:
+        if i in completion_dic:
+            completion_dic[i] += 1
+            continue
+        completion_dic[i] = 1
+    while participant:
+        select = participant.pop()
+        if select not in completion_dic:
+            answer = select
+            break
+        completion_dic[select] -= 1
+    return answer
+
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
