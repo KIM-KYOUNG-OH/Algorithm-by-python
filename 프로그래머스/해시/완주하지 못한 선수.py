9f29dd9b6aa0a93b7c58f8417937b1db2e8731df
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-18 23:48:19 +0900

    알고리즘풀이

--- 프로그래머스/해시/완주하지 못한 선수.py
+++ 프로그래머스/해시/완주하지 못한 선수.py
@@ -9,21 +9,21 @@
 
 def solution(participant, completion):
     answer = ''
-    completion_dic = dict()
+    completion_dic = dict()  # 해시
     for i in completion:
-        if i in completion_dic:
+        if i in completion_dic:  # 이름이 중복되서 나오면 value +1
             completion_dic[i] += 1
             continue
-        completion_dic[i] = 1
+        completion_dic[i] = 1  # 처음 나온 선수면 해시에 추가
     while participant:
-        select = participant.pop()
-        if select not in completion_dic:
+        select = participant.pop()  # 참가자중 한명 뽑기
+        if select not in completion_dic:  # 해시에 없으면 완주 못한 사람이므로 출력후 함수 종료
             answer = select
             break
-        completion_dic[select] -= 1
-        if completion_dic[select] == 0:
+        completion_dic[select] -= 1  # 해시에 있으면 value -1
+        if completion_dic[select] == 0:  # value가 0이 되면 해시에서 삭제
             del completion_dic[select]
-    return answer  
+    return answer
 
 
 print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
--- 프로그래머스/해시/전화번호 목록.py
+++ 프로그래머스/해시/전화번호 목록.py
@@ -1,13 +1,24 @@
+# def solution(phone_book):
+#     phone_book.sort()
+#     for i in range((len(phone_book))):
+#         key = phone_book[i] # key = phone_book의 요소
+#         temp = list(phone_book)
+#         temp.pop(i)
+#         for j in range(len(temp)):
+#             if key == temp[j][:len(key)]: # phone_book 요소중 key의 길이만큼 문자열을 slicing
+#                 return False
+#     return True
+
 def solution(phone_book):
-    phone_book.sort()
-    for i in range((len(phone_book))):
-        key = phone_book[i] # key = phone_book의 요소
-        temp = list(phone_book)
-        temp.pop(i)
-        for j in range(len(temp)):
-            if key == temp[j][:len(key)]: # phone_book 요소중 key의 길이만큼 문자열을 slicing
-                return False
-    return True
+    answer = True
+    phone_book.sort(key=lambda x: len(x))
+    for i in range(0, len(phone_book) - 1):
+        for j in range(i + 1, len(phone_book)):
+            for k in range(len(phone_book[i])):
+                if phone_book[i][k] != phone_book[j][k]:
+                    break
+                
+    return answer
 
 print(solution(["119", "97674223", "1195524421"]))
 
