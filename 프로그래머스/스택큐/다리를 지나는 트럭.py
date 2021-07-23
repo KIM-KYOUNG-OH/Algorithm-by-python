f03aab55ad850adeb8e9afe5f8de13d3f7a114c5
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-23 21:49:29 +0900

    알고리즘풀이

--- 프로그래머스/스택큐/다리를 지나는 트럭.py
+++ 프로그래머스/스택큐/다리를 지나는 트럭.py
@@ -1,19 +1,20 @@
 from collections import deque
 
-def solution(bridge_length, weight, truck_weights):
-    bridge = [0] * bridge_length
-    q = deque(bridge)
-    tw = deque(truck_weights)
-    second = 0
-    while q:
-        second += 1
-        q.popleft()
-        if tw:
-            if sum(q) + tw[0] <= weight:
-                q.append(tw.popleft())
-            else:
-                q.append(0)
 
-    return second
+def solution(bridge_length, weight, truck_weights):  # 다리가 견딜 수 있는 최대 트럭수, 최대 무게, 트럭 무게 리스트
+    crossing = [0] * bridge_length  # 다리를 건너는 트럭
+    waiting = deque(truck_weights)  # 대기중인 트럭
+    time = 0  # 현재 시간
+    while crossing:
+        time += 1
+        crossing.pop(0)
+        if waiting:
+            if sum(crossing) + waiting[0] <= weight:
+                crossing.append(waiting.popleft())
+            else:
+                crossing.append(0)
+
+    return time
+
 
 print(solution(2,10,[7,4,5,6]))
