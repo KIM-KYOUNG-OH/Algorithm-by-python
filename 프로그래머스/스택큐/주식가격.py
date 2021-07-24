7129adc35238cc6f9db2bca64639937af9c2d153
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-24 20:58:32 +0900

    알고리즘풀이

--- 프로그래머스/스택큐/주식가격.py
+++ 프로그래머스/스택큐/주식가격.py
@@ -1,14 +1,28 @@
-def solution(prices):
-    answer = [0] * len(prices)
+# def solution(prices):
+#     answer = [0] * len(prices)
+#
+#     for i in range(len(prices)-1):
+#         for j in range(i, len(prices)-1):
+#             print(i, j)
+#             if prices[i] > prices[j+1]:
+#                 answer[i] += 1
+#                 break
+#             else:
+#                 answer[i] +=1
+#     return answer
 
-    for i in range(len(prices)-1):
-        for j in range(i, len(prices)-1):
-            print(i, j)
-            if prices[i] > prices[j+1]:
-                answer[i] += 1
+from collections import deque
+
+
+def solution(prices):
+    answer = [0] * len(prices)  # 가격이 떨어지지 않은 연속 횟수 전부 0으로 초기화
+    for i in range(len(prices) - 1):  # 마지막은 항상 0이므로 len(prices) - 1 번까지 순회
+        for j in range(i, len(prices) - 1):
+            answer[i] += 1
+            if prices[i] > prices[j + 1]:  # 가격이 떨어졌을 경우 반복문 탈출
                 break
-            else:
-                answer[i] +=1
+
     return answer
+
 
 print(solution([1, 2, 3, 2, 3]))
