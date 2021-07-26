d7355184c12dc315504b2a045016de13a39ab7a0
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-26 23:47:33 +0900

    알고리즘풀이

--- 프로그래머스/정렬/K번째수.py
+++ 프로그래머스/정렬/K번째수.py
@@ -1,10 +1,21 @@
+# def solution(array, commands):
+#     answer = []
+#     temp = []
+#     for i in range(len(commands)):
+#         temp = array[commands[i][0] - 1: commands[i][1]]
+#         temp.sort()
+#         answer.append(temp[commands[i][2] - 1])
+#     return answer
+def pick(array, i, j, k):
+    array_fixed = array[i - 1: j]
+    array_fixed.sort()
+    return array_fixed[k - 1]
+
+
 def solution(array, commands):
     answer = []
-    temp = []
-    for i in range(len(commands)):
-        temp = array[commands[i][0] - 1: commands[i][1]]
-        temp.sort()
-        answer.append(temp[commands[i][2] - 1])
+    for command in commands:
+        answer.append(pick(array, command[0], command[1], command[2]))
     return answer
 
 
