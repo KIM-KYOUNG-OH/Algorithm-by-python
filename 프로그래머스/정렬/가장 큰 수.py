76fc7ba0136c907313d83015f341ceda7f922c7a
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-29 00:03:32 +0900

    알고리즘풀이

--- 프로그래머스/정렬/가장 큰 수.py
+++ 프로그래머스/정렬/가장 큰 수.py
@@ -4,9 +4,9 @@
 #     return str(int(''.join(numbers_str)))
 
 def solution(numbers):
-    numbers_str = list(map(str, numbers))
-    numbers_str.sort(key=lambda x: x * 3, reverse=True)
-    return str(int(''.join(numbers_str)))
+    numbers_str = list(map(str, numbers))  # 문자열 타입으로 변환
+    numbers_str.sort(key=lambda x: x * 3, reverse=True)  # 성분이 최대 1000이므로 최소 3자리수까지 맞춰서 비교
+    return str(int(''.join(numbers_str)))  # 배열 성분들 문자열로 합치기
 
 
 print(solution([3, 30, 34, 5, 9]))
