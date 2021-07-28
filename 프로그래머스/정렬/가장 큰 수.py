8b254cefd544bfe159ff1f1445ce746386603092
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-07-28 23:55:00 +0900

    알고리즘풀이

--- 프로그래머스/정렬/가장 큰 수.py
+++ 프로그래머스/정렬/가장 큰 수.py
@@ -1,6 +1,11 @@
+# def solution(numbers):
+#     numbers_str = list(map(str, numbers))
+#     numbers_str.sort(key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers_str)))
+
 def solution(numbers):
     numbers_str = list(map(str, numbers))
-    numbers_str.sort(key=lambda x: x*3, reverse=True)
+    numbers_str.sort(key=lambda x: x * 3, reverse=True)
     return str(int(''.join(numbers_str)))
 
 
--- 프로그래머스/힙/디스크 컨트롤러.py
+++ 프로그래머스/힙/디스크 컨트롤러.py
@@ -1,39 +1,8 @@
-# import heapq
-#
-# def solution(jobs):
-#     answer,now,i = 0,0,0
-#     start = -1
-#     heap = []
-#
-#     while i < len(jobs):
-#         for j in jobs:
-#             if start < j[0] <= now:
-#                 heapq.heappush(heap, [j[1], j[0]])
-#                 # [소요시간, 시작시간]
-#         if len(heap) > 0:
-#             current = heapq.heappop(heap)
-#             start = now
-#             now += current[0]
-#             answer += (now-current[1])
-#             i += 1
-#         else:
-#             now += 1
-#     return int(answer/len(jobs))
 def solution(jobs):
-    answer = 0
-    jobs_sorted = sorted(jobs, key=lambda x: x[1])  # stack, 작업 소요 시간 기준 내림차순 정렬
-    time = 0  # 현재 시간
-    while jobs_sorted:
-        job = jobs_sorted.pop()
-        if time <= job[0]:
-            answer += time + job[1]
-            time = job[0] + job[1]
-            continue
-        if time > job[0]:
-            answer += job[1]
-            time = (job[0] + time)
+    answer, now, i = 0, 0, 0  # 총 작업시간, 현재시간, jobs의 index
+    heap = []
 
-    return answer
+    return int(answer/len(jobs))
 
 
 print(solution([[0, 3], [1, 9], [2, 6]]))
