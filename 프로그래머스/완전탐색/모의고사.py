b1db423f9358d06d2b122ae0638078bb664ba69e
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-08-01 10:54:19 +0900

    알고리즘풀이

--- 프로그래머스/완전탐색/모의고사.py
+++ 프로그래머스/완전탐색/모의고사.py
@@ -1,25 +1,46 @@
+# def solution(answers):
+#     answer = []
+#     correct_one = 0
+#     correct_two = 0
+#     correct_three = 0
+#
+#     one = [1,2,3,4,5]
+#     two = [2,1,2,3,2,4,2,5]
+#     three = [3,3,1,1,2,2,4,4,5,5]
+#
+#     for i in range(len(answers)):
+#         if answers[i] == one[i%len(one)]:
+#             correct_one += 1
+#         if answers[i] == two[i%len(two)]:
+#             correct_two += 1
+#         if answers[i] == three[i%len(three)]:
+#             correct_three += 1
+#     temp = [correct_one, correct_two, correct_three]
+#
+#     for key, value in enumerate(temp):
+#         if max(temp) == value:
+#             answer.append(key+1)
+#     return answer
+
 def solution(answers):
-    answer = []
-    correct_one = 0
-    correct_two = 0
-    correct_three = 0
-
-    one = [1,2,3,4,5]
-    two = [2,1,2,3,2,4,2,5]
-    three = [3,3,1,1,2,2,4,4,5,5]
-
+    person1 = [1, 2, 3, 4, 5]
+    person2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    cnts = [0, 0, 0]  # 1, 2, 3번 수포자의 정답 횟수
     for i in range(len(answers)):
-        if answers[i] == one[i%len(one)]:
-            correct_one += 1
-        if answers[i] == two[i%len(two)]:
-            correct_two += 1
-        if answers[i] == three[i%len(three)]:
-            correct_three += 1
-    temp = [correct_one, correct_two, correct_three]
+        if answers[i] == person1[i % 5]:
+            cnts[0] += 1
+        if answers[i] == person2[i % 8]:
+            cnts[1] += 1
+        if answers[i] == person3[i % 10]:
+            cnts[2] += 1
 
-    for key, value in enumerate(temp):
-        if max(temp) == value:
-            answer.append(key+1)
+    answer = []
+    for i, cnt in enumerate(cnts):
+        if cnt == max(cnts):
+            answer.append(i + 1)
+
     return answer
+
 
 print(solution([1,3,2,4,2]))
--- 프로그래머스/완전탐색/소수찾기.py
+++ 프로그래머스/완전탐색/소수찾기.py
@@ -1,26 +1,53 @@
-from itertools import permutations
+# from itertools import permutations
+#
+# def count_prime(num_case):
+#     prime_count = 0
+#     for n in num_case:
+#         count = 0
+#         for i in range(2, n): # 2부터 n-1까지 나누었을때 나머지가 전부 0이 아니면 소수
+#             if n%i == 0:
+#                 count+=1
+#                 break
+#         if n>1 and count==0:
+#             prime_count += 1
+#     return prime_count
+#
+# def solution(numbers):
+#     num_case = []
+#     for i in range(1,len(numbers)+1):
+#         tmp = permutations(numbers, i)
+#
+#         for n in tmp:
+#             tmp_str = "".join(n)
+#             num_case.append(int(tmp_str))
+#     num_case = list(set(num_case)) # 중복 없앰
+#     return count_prime(num_case)
+import itertools
 
-def count_prime(num_case):
-    prime_count = 0
-    for n in num_case:
-        count = 0
-        for i in range(2, n): # 2부터 n-1까지 나누었을때 나머지가 전부 0이 아니면 소수
-            if n%i == 0:
-                count+=1
-                break
-        if n>1 and count==0:
-            prime_count += 1
-    return prime_count
 
 def solution(numbers):
-    num_case = []
-    for i in range(1,len(numbers)+1):
-        tmp = permutations(numbers, i)
+    is_prime = [True] * int(1e7)  # 1 ~ 9999999 까지 소수판별 리스트 선언
+    is_prime[0], is_prime[1] = False, False
+    for i in range(2, int(len(is_prime) ** 0.5) + 1):  # 에라토스테네스의 체
+        if not is_prime[i]:
+            continue
+        for j in range(i + i, len(is_prime), i):
+            if is_prime[j]:
+                is_prime[j] = False
 
-        for n in tmp:
-            tmp_str = "".join(n)
-            num_case.append(int(tmp_str))
-    num_case = list(set(num_case)) # 중복 없앰
-    return count_prime(num_case)
+    numbers = list(numbers)
+    candidates = set()  # 모든 경우의 수
+    for i in range(1, len(numbers) + 1):
+        cases = list(itertools.permutations(numbers, i))  # 순열로 i만큼의 길이를 가진 모든 경우의 수 정의
+        for case in cases:
+            candidates.add(int(''.join(case)))  # 문자 리스트에서 int로 변환하여 저장
+
+    answer = 0  # 소수의 개수
+    for candidate in list(candidates):
+        if is_prime[candidate]:  # 미리 찾아놓은 소수 판별 리스트에 대조
+            answer += 1
+
+    return answer
+
 
 print(solution("17"))
