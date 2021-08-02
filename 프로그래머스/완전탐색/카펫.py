28cb47e254a54a342df7e548d71ca89188158962
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-08-02 23:32:06 +0900

    알고리즘풀이

--- 프로그래머스/완전탐색/카펫.py
+++ 프로그래머스/완전탐색/카펫.py
@@ -1,10 +1,19 @@
+# def solution(brown, yellow):
+#     answer=[]
+#     x = (4+brown+((4+brown)**2-16*(brown+yellow))**0.5)/4
+#     y = (brown+yellow)/x
+#     temp = [int(x),int(y)]
+#     answer.append(max(temp))
+#     answer.append(min(temp))
+#     return answer
+
 def solution(brown, yellow):
-    answer=[]
-    x = (4+brown+((4+brown)**2-16*(brown+yellow))**0.5)/4
-    y = (brown+yellow)/x
-    temp = [int(x),int(y)]
-    answer.append(max(temp))
-    answer.append(min(temp))
+    answer = []
+    # 연립 이차방정식의 근의 공식
+    x = int((brown + 4 + ((brown + 4) ** 2 - 16 * (brown + yellow)) ** 0.5) / 4)
+    y = int((brown + yellow) / x)
+    answer = [max(x, y), min(x, y)]
     return answer
+
 
 print(solution(10, 2))
