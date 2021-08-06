fc5117150e31d28582b30ed894d4b829ae90feca
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-08-06 21:25:16 +0900

    알고리즘풀이

--- 프로그래머스/그리디/큰 수 만들기.py
+++ 프로그래머스/그리디/큰 수 만들기.py
@@ -1,18 +1,36 @@
+# def solution(number, k):
+#     stack = []
+#     for i in number:
+#         while stack and i > stack[-1]:
+#             if k > 0:
+#                 stack.pop()
+#                 k -= 1
+#             else:
+#                 break
+#         stack.append(i)
+#
+#     if k > 0:
+#         for _ in range(k):
+#             stack.pop()
+#
+#     return "".join(stack)
+
 def solution(number, k):
     stack = []
-    for i in number:
-        while stack and i > stack[-1]:
+    for num in number:  # number의 앞에서부터 작은값 k개 뽑기
+        while stack and num > stack[-1]:
             if k > 0:
                 stack.pop()
                 k -= 1
             else:
                 break
-        stack.append(i)
+        stack.append(num)
 
-    if k > 0:
-        for _ in range(k):
-            stack.pop()
+    while k > 0:  # number가 오름차순이라 삭제할 횟수가 남았을 때
+        stack.pop()
+        k -= 1
 
-    return "".join(stack)
+    return ''.join(stack)
+
 
 print(solution("4177252841",4))
