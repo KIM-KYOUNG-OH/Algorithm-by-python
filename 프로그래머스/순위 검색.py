08a5c8f05629cbd4b4814c49eee91f2dcda2ef33
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-11-12 20:04:10 +0900

    알고리즘풀이

--- 프로그래머스/순위 검색.py
+++ 프로그래머스/순위 검색.py
@@ -1,30 +1,46 @@
+from bisect import bisect_left
+from itertools import combinations
+
+
 def solution(info, query):
     answer = []
-    for question in query:
-        cnt = len(info)
-        temp = list(question.split(' '))
-        while 'and' in temp:
-            temp.remove('and')
-        for i in range(len(info)):
+    info_dict = dict()
 
-            grade = info[i].split(' ')[-1]
-            if int(grade) < int(temp[-1]):
-                cnt -= 1
-                # print("점수 미달 - cnt : ", cnt)
-                continue
-            for j in range(4):
-                # print(temp[j])
-                if temp[j] == "-":
-                    continue
-                if temp[j] not in info[i]:
-                    cnt -= 1
-                    # print("cnt : ", cnt)
-                    break
+    for i in range(len(info)):
+        candidate = info[i].split()
+        key = candidate[:-1]
+        value = int(candidate[-1])
 
-        answer.append(cnt)
-        # print(answer)
+        for j in range(5):
+            for candidates in combinations(key, j):
+                key_fixed = ''.join(sorted(candidates))
+                if key_fixed not in info_dict:
+                    info_dict[key_fixed] = [value]
+                else:
+                    info_dict[key_fixed].append(value)
 
+    for k in info_dict:
+        info_dict[k].sort()
+
+    for qu in query:
+        candidate = qu.split(' ')
+        key = candidate[:-1]
+        value = int(candidate[-1])
+
+        while 'and' in key:
+            key.remove('and')
+        while '-' in key:
+            key.remove('-')
+        key_fixed = ''.join(sorted(key))
+
+        if key_fixed in info_dict:
+            scores = info_dict[key_fixed]
+            except_idx = bisect_left(scores, value)
+            answer.append(len(scores) - except_idx)
+        else:
+            answer.append(0)
     return answer
+
 
 print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],
                ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
