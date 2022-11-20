25eb1286aeb34e13f5bd196483d5f0fb49900182
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-11-20 22:32:11 +0900

    11/20

--- 프로그래머스/level2/이진 변환 반복하기.py
+++ 프로그래머스/level2/이진 변환 반복하기.py
@@ -1,3 +1,22 @@
 def solution(s):
-    answer = []
-    return answer
+    return recursive(s)
+
+
+def recursive(s):
+    convert_cnt, zero_cnt = 0, 0
+    while True:
+        if s == '1':
+            answer = [convert_cnt, zero_cnt]
+            return answer
+        zero = 0
+        for ch in list(s):
+            if ch == '0':
+                zero += 1
+
+        fixed_s = s.replace('0', '')
+        convert_cnt += 1
+        zero_cnt += zero
+        s = bin(len(fixed_s))[2:]
+
+
+solution("110010101001")
