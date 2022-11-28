9c214ecd932f9edb90303fd3b96ea77c3a700ea4
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-11-28 16:35:16 +0900

    11/28

--- 이것이 코딩테스트다/chap7-2.py
+++ 이것이 코딩테스트다/chap7-2.py
@@ -1,33 +1,63 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# def binary_search(target, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if array[mid] == target:
+#             return True
+#         elif array[mid] > target:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return False
+#
+# n = int(input())
+# array = list(map(int, input().split()))
+# m = int(input())
+# requests = list(map(int, input().split()))
+# array.sort()
+# for i in requests:
+#     if binary_search(i, 0, n-1):
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+#
+# # input
+# # 5
+# # 8 3 7 9 2
+# # 3
+# # 5 7 9
+# #
+# # output
+# # no yes yes
 
-def binary_search(target, start, end):
+import sys
+
+
+n = int(sys.stdin.readline().rstrip())
+parts = list(map(int, sys.stdin.readline().rstrip().split()))
+m = int(sys.stdin.readline().rstrip())
+finds = list(map(int, sys.stdin.readline().rstrip().split()))
+parts.sort()
+
+
+def binary_search(arr: list, target: int) -> int:
+    start, end = 0, len(arr) - 1
     while start <= end:
         mid = (start + end) // 2
-        if array[mid] == target:
-            return True
-        elif array[mid] > target:
-            end = mid - 1
-        else:
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
             start = mid + 1
-    return False
+        else:
+            end = mid - 1
+    return -1
 
-n = int(input())
-array = list(map(int, input().split()))
-m = int(input())
-requests = list(map(int, input().split()))
-array.sort()
-for i in requests:
-    if binary_search(i, 0, n-1):
-        print('yes', end=' ')
+
+for find in finds:
+    idx = binary_search(parts, find)
+    if idx == -1:
+        print('no')
     else:
-        print('no', end=' ')
-
-# input
-# 5
-# 8 3 7 9 2
-# 3
-# 5 7 9
-#
-# output
-# no yes yes
+        print('yes')
--- 이것이 코딩테스트다/chap7-3.py
+++ 이것이 코딩테스트다/chap7-3.py
@@ -1,25 +1,50 @@
-import  sys
-input = sys.stdin.readline
+# import  sys
+# input = sys.stdin.readline
+#
+# def binary_search(start, end):
+#     ans = 0
+#     while start <= end:
+#         mid = (start + end) // 2
+#         total_length = 0
+#         for cake in rice_cakes:
+#             if mid < cake:
+#                 total_length += (cake - mid)
+#         if total_length == m:
+#             return mid
+#         elif total_length > m:
+#             start = mid + 1
+#             ans = max(ans, mid)
+#         else:
+#             end = mid - 1
+#             ans = max(ans, mid)
+#     return ans
+#
+# n,m = map(int, input().split())
+# rice_cakes = list(map(int, input().split()))
+# rice_cakes.sort()
+# print(binary_search(0, max(rice_cakes)))
 
-def binary_search(start, end):
-    ans = 0
-    while start <= end:
-        mid = (start + end) // 2
-        total_length = 0
-        for cake in rice_cakes:
-            if mid < cake:
-                total_length += (cake - mid)
-        if total_length == m:
-            return mid
-        elif total_length > m:
-            start = mid + 1
-            ans = max(ans, mid)
-        else:
-            end = mid - 1
-            ans = max(ans, mid)
-    return ans
+import sys
 
-n,m = map(int, input().split())
-rice_cakes = list(map(int, input().split()))
-rice_cakes.sort()
-print(binary_search(0, max(rice_cakes)))
+n, m = map(int, sys.stdin.readline().rstrip().split())
+lst = list(map(int, sys.stdin.readline().rstrip().split()))
+lst.sort()
+
+start, end = 0, max(lst)
+
+result = 0
+
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    for x in lst:
+        if x > mid:
+            total += x - mid
+
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+
