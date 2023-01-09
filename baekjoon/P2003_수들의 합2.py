5b3c0aaec2078b927503647a9122cafe0a1e370c
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2023-01-09 14:04:33 +0900

    1/8

--- baekjoon/P2003_수들의 합2.py
+++ baekjoon/P2003_수들의 합2.py
@@ -1,17 +1,30 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().rstrip().split())
-numbers = list(map(int, sys.stdin.readline().rstrip().split()))
-answer = 0
-for i in range(n):
-    total = 0
-    j = i
-    while j < n:
-        total += numbers[j]
-        if total > m:
-            break
-        if total == m:
-            answer += 1
-            break
-        j += 1
-print(answer)
+nums = list(map(int, sys.stdin.readline().rstrip().split()))
+left, right = 0, 1
+cnt = 0
+while left <= right <= n:
+    sum_nums = nums[left:right]
+    total = sum(sum_nums)
+
+    if total == m:
+        cnt += 1
+        right += 1
+    elif total < m:
+        right += 1
+    else:
+        left += 1
+print(cnt)
+
+# answer = 0
+# for i in range(n):
+#     total = 0
+#     for j in range(i, n):
+#         total += nums[j]
+#         if total == m:
+#             answer += 1
+#             break
+#         elif total > m:
+#             break
+# print(answer)
