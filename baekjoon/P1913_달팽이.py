5e383e4863784089f31d6c0b9ad7eda5286ed18e
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2023-01-22 00:12:21 +0900

    1/21

--- baekjoon/P1913_달팽이.py
+++ baekjoon/P1913_달팽이.py
@@ -3,23 +3,54 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 target = int(sys.stdin.readline().rstrip())
 matrix = [[0] * (n + 1) for _ in range(n + 1)]
-cy, cx = 0, 0
-if n == 3:
-    cy, cx = 1, 1
-else:
-    cy, cx = n // 2 + 1, n // 2 + 1
+cy, cx = n // 2 + 1, n // 2 + 1
 num = 1
 matrix[cy][cx] = num
-startY = cy - 1
-startX = cx
+y = cy - 1
+x = cx
+answer = [cy, cx]
 for depth in range(1, n // 2 + 1):
     l = 2 * depth + 1
     # 위
-    for i in range(l - 1):
-
+    for _ in range(l - 2):
+        num += 1
+        matrix[y][x] = num
+        if num == target:
+            answer = [y, x]
+        x += 1
 
     # 오른
+    for _ in range(l - 1):
+        num += 1
+        matrix[y][x] = num
+        if num == target:
+            answer = [y, x]
+        y += 1
 
     # 아래
+    for _ in range(l - 1):
+        num += 1
+        matrix[y][x] = num
+        if num == target:
+            answer = [y, x]
+        x -= 1
 
     # 왼
+    for _ in range(l):
+        num += 1
+        matrix[y][x] = num
+        if num == target:
+            answer = [y, x]
+        y -= 1
+
+    # for i in range(1, n + 1):
+    #     for j in range(1, n + 1):
+    #         print(matrix[i][j], end=' ')
+    #     print()
+
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(matrix[i][j], end=' ')
+    print()
+print(answer[0], answer[1])
