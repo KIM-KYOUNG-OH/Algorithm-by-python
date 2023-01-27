be9827403083c11b1b8aead8129399ba40d3d6c7
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2023-01-27 17:20:08 +0900

    1/27

---
+++ baekjoon/P24479_깊이 우선 탐색 1.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque, defaultdict
+
+
+def dfs(n, cur):
+    path.append(cur)
+
+    for node in graph[cur]:
+        if not visit[node]:
+            visit[node] = True
+            dfs(n, node)
+    return
+
+
+n, m, r = map(int, sys.stdin.readline().rstrip().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().rstrip().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for i in range(1, n + 1):
+    graph[i].sort()
+
+path = deque()
+visit = [False] * (n + 1)
+visit[r] = True
+dfs(n, r)
+
+answer = defaultdict(int)
+idx = 1
+while path:
+    cur = path.popleft()
+    answer[cur] = idx
+    idx += 1
+
+for i in range(1, n + 1):
+    print(answer[i])
