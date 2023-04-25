9a5951063c39680ae2f319a7041476df12da69ff
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2023-04-26 03:12:25 +0900

    4/26

--- LeetCode/Number of Islands.py
+++ LeetCode/Number of Islands.py
@@ -1,33 +1,30 @@
-import sys
-from typing import List
-n = int(input())
-matrix = [list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
+from collections import deque
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
 
-def dfs(grid: List[List[str]], i: int, j:int) -> None:
-    # 탐색 인덱스가 바깥쪽일 경우
-    if i < 0 or i >= len(grid[0]) or \
-            j < 0 or j >=len(grid[0]) or \
-            grid[i][j] != 1: # 탐색 인덱스가 섬이 아닌 경우
-        return
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n, m = len(grid), len(grid[0])
+        visit = [[False] * m for _ in range(n)]
+        answer = 0
+        for i in range(n):
+            for j in range(m):
+                if visit[i][j] or grid[i][j] == '0':
+                    continue
 
-    grid[i][j] = '#' # 이미 탐색이 된 인덱스는 # 등의 다른 부호로 바꿔주어 이미 방문을 했다는 표시를 남긴다. 이러면 다시 계산할 필요가 없게 된다.(가지치기)
-
-    # 호출 됐는데 위의 조건에 걸리면 리턴되면서 종료된다. (백트래킹으로 빠져나와 그 전 지점으로 돌아온다.)
-    dfs(grid,i+1,j) # 재귀로 인접한 영역 탐색(아래쪽 방향)
-    dfs(grid,i-1,j) # 재귀로 인접한 영역 탐색(위쪽 방향)
-    dfs(grid,i,j+1) # 재귀로 인접한 영역 탐색(오른쪽 방향)
-    dfs(grid,i,j-1) # 재귀로 인접한 영역 탐색(왼쪽 방향)
-
-if not matrix: # 행렬이 없는 경우 예외처리
-    exit()
-
-count = 0 # 섬 개수를 위한 변수
-
-for i in range(len(matrix)): # 행을 탐색
-    for j in range(len(matrix[0])): # 열을 탐색
-        if matrix[i][j] == 1: # 한 줄씩 탐색하면서 현재 위치가 섬이면
-            dfs(matrix,i,j) # dfs 호출해서 상하좌우도 섬인지 탐색
-            count+=1 #위의 함수(재귀 포함)를 다 돌고 더 이상 돌릴 재귀가 없으면 카운트를 +1
-
-print(count)
+                q = deque()
+                q.append((i, j))
+                visit[i][j] = True
+                answer += 1
+                while q:
+                    y, x = q.popleft()
+                    for k in range(4):
+                        ny = y + dy[k]
+                        nx = x + dx[k]
+                        if 0 <= ny < n and 0 <= nx < m:
+                            if not visit[ny][nx] and grid[ny][nx] == '1':
+                                visit[ny][nx] = True
+                                q.append((ny, nx))
+        return answer
