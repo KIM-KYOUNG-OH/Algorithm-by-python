9b80f4e1815db9ca2af95fcf724d837a495008cb
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2023-04-27 23:44:50 +0900

    4/27

--- LeetCode/Reverse Linked List.py
+++ LeetCode/Reverse Linked List.py
@@ -3,20 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from collections import deque
+
+
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        # 함수 정의
-        def reverse(node: ListNode, prev: ListNode = None):
-            # base case
-            if not node:
-                return prev
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        q = deque()
+        cur = head
+        while True:
+            if cur is None:
+                break
+            q.append(cur)
+            cur = cur.next
 
-            # recursive case
-            # next 변수에 현재 노드의 next를 넣고 매개변수 node로 재귀 호출, node.next에는 이전 호출의 노드를 재할당
-            next, node.next = node.next, prev
-            # next가 재귀 호출 시 매개변수 node 자리로 들어감, 현재 node는 prev 자리로 들어감
-            return reverse(next, node)
-
-            # 함수 실행
-
-        return reverse(head)
+        if not q:
+            return head
+        top = q.pop()
+        answer = top
+        while q:
+            last = q.pop()
+            top.next = last
+            top = last
+        top.next = None
+        return answer
