663fde5bf74364d6b1a1823aa4f87aef3d7fff7c
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2023-05-10 23:47:07 +0900

    5/10

--- LeetCode/Lowest Common Ancestor of a Binary Search Tree.py
+++ LeetCode/Lowest Common Ancestor of a Binary Search Tree.py
@@ -1,3 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if root.val == p.val or root.val == q.val:
